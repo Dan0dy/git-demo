d61d241c1b5027ef57491123ca26ff51962a8ced
Dan0dy <49503853+Dan0dy@users.noreply.github.com>
2022-06-18 22:33:31 +0800

    Update LeetCode.java

--- LeetCode.java
+++ LeetCode.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class LeetCode {
     public static void main(String[] args) {
         System.out.println("\" +\"");
-        System.out.println;
+        System.out.println();
     }
 }
 
